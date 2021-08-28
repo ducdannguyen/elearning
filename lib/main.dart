4c77750a90da32737fae60568ba7786f197afef0
Dan Nguyen <ducdannguyen@gmail.com>
2021-08-28 19:13:46 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,13 @@ import 'package:elearning/screens/home_screen.dart';
 import 'package:elearning/utils/const.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(EraApp());
 
-class MyApp extends StatelessWidget {
+class EraApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'E-Learning Online Courses App',
+      title: 'ERA App',
       debugShowCheckedModeBanner: false,
       theme: Constants.lighTheme(context),
       home: HomeScreen(),
